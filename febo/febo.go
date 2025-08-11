cab8204466e19b98a3cc99113c17151edb9696a6
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2025-08-11 23:36:39 +0530

    fedo with tabulate method

--- febo/febo.go
+++ febo/febo.go
@@ -19,6 +19,19 @@ return febonacci(num-2)+febonacci(num-1)
 
 
 }
+func febotib(num int)int{
+	array := make([]int, num+1)
+	array[1]=1
+	for i:=0;i<=num;i++{
+		if i+1 <= num {
+			array[i+1] += array[i]
+		}
+		if i+2 <= num {
+			array[i+2] += array[i]
+		}
+}
+
+		return array[num]}
 func main(){
 
 fmt.Println("febonacci in recursion")
@@ -27,8 +40,10 @@ input,_:=reader.ReadString('\n')
 input=strings.TrimSpace(input)
 num,_:=strconv.Atoi(input)
 
-	memo:=make(map[int]int)
-fmt.Println(febowithmemo(num,memo))
+
+//	memo:=make(map[int]int)
+//fmt.Println(febowithmemo(num,memo))
+fmt.Println(febotib(num))
 } 
 
 func febowithmemo(num int, memo map[int]int)int{
