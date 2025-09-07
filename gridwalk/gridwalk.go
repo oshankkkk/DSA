36bccb762bda481b0707efa5c0a8edb50f4a1ec1
Oshan Kodagoda <oshankodagoda200667@gmail.com>
2025-09-07 21:47:41 +0530

    grid walker tabulation

--- gridwalk/gridwalk.go
+++ gridwalk/gridwalk.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 func main(){
-	grid:=make(map[string]int)
-	a:=gridtraveler(3,3,grid)
+//	grid:=make(map[string]int)
+//	a:=gridtraveler(3,3,grid)
 
-	b:=gridtraveler(18,18,grid)
-fmt.Println(a)
+//	b:=gridtraveler(18,18,grid)
+//fmt.Println(a)
 
-fmt.Println(b)
+//fmt.Println(b)
+gridtravelertabulation(3,3)
 }
 func gridtraveler(row,col int,grid map[string]int)int{
 	key:=fmt.Sprintf("%d , %d",row,col)
@@ -26,4 +27,44 @@ grid[key]=gridtraveler(row-1,col,grid)+gridtraveler(row,col-1,grid)
 
 }
 return grid[key]
+}
+func gridtravelertabulation(rows,cols int){
+	grid:=make([][]int,rows+1)
+	for index,_:=range grid{
+		grid[index]=make([]int,cols+1)
+	}
+	for index,_:=range grid{
+		for innerindex,_:=range grid[index]{
+			grid[index][innerindex]=0
+		}
+
+	}
+
+
+grid[1][1]=1
+for i:= 1;i<=rows;i++{
+	for j:=1;j<=cols;j++{
+		if i==1 && j==1{
+continue
+		}
+	grid[i][j]+=grid[i-1][j]+grid[i][j-1]
+
+
+
+	}
+
+}
+
+
+
+
+
+	for _,value:=range grid{
+		for _,innerval:=range value{
+			fmt.Print(innerval)
+		}
+		fmt.Println("")
+
+	}
+
 }
